f8eace6c9e8974dfb6169b03088a7f0d2dd6e11d
luis enrique sauceda herrera <luisesh2@hotmail.com>
2017-03-29 16:42:24 -0600

    dos

--- examen.hs
+++ examen.hs
@@ -1,2 +1,6 @@
 buscarElemento (x:xs)  ele num = 
  (ele == head (drop (num-1)  (x:xs)))
+
+cambiarElemento (x:xs) num = 
+ 
+ head (drop (num-1)  (x:xs)):(take (num-1) (x:xs)) ++ ((drop (num)  (x:xs)))
