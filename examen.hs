55b15a3b51bd51f1fcedd6e4c838be4f2c80d069
luis enrique sauceda herrera <luisesh2@hotmail.com>
2017-03-29 16:52:33 -0600

    tres

--- examen.hs
+++ examen.hs
@@ -4,3 +4,8 @@ buscarElemento (x:xs)  ele num =
 cambiarElemento (x:xs) num = 
  
  head (drop (num-1)  (x:xs)):(take (num-1) (x:xs)) ++ ((drop (num)  (x:xs)))
+
+diferencia [] = error "no se puede sacar direrencia"
+diferencia [x] = []
+diferencia (x:xs) = ((head xs)-x ):diferencia (xs)
+ 
